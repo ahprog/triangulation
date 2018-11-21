f4f9983f2bbbd0b4e11bac09126861d5b6b6f62d
ahprog <herrero.antonin@gmail.com>
2018-11-21 16:45:31 +0100

    Admin du projet Maven

--- src/main/java/triangulation/App.java
+++ src/main/java/triangulation/App.java
@@ -1,4 +1,4 @@
-package com.algo.devoir3;
+package triangulation;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello le monDE!" );
     }
 }
